535c5d658e3831e20bb46951d532faccf83507b1
Mark <mark.kapkan@gmail.com>
2024-06-11 11:14:22 +0300

    Fixed second game

--- js/guess-the-number.js
+++ js/guess-the-number.js
@@ -2,13 +2,14 @@ let inputGuess = document.querySelector('.guess-the-number__input')
 let submitButton = document.querySelector('.guess-the-number__button')
 let clue = document.querySelector('.guess-the-number__text')
 let randomNumber = Math.floor(Math.random()*100+1)
+let tries = 0
 submitButton.addEventListener('click', function(values){
     values = parseInt(inputGuess.value)
-    let tries = 0
     if(values == randomNumber){
         clue.style.opacity = '1'
         clue.style.color = 'green'
         clue.textContent = `Вітаємо! ви угадали з ${tries} спроби!`
+        tries = 0
     }else if(values < randomNumber){
         clue.style.opacity = '1'
         clue.style.color = 'red'
